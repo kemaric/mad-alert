044796bc64a9d673c2bacd002bd5239142610e6d
elysham13@gmail.com <elysham13@gmail.com@92224ab3-2fd2-0d66-1742-4fb4694664a7>
2013-03-15 22:26:19 +0000

    No changes to functionality but now we are using an AsyncTask to do all the heavy work of  loading a Url and parsing it using the AsyncTask is important so the app works on newer devises

--- trunk/MadAlerts/src/com/example/madalerts/MainActivity.java
+++ trunk/MadAlerts/src/com/example/madalerts/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.madalerts;
 
-
-
 import java.util.ArrayList;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -14,16 +13,19 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	public final String DEFAULT_RSS_URL= "https://alert.umd.edu/rssfeed.php";
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+	private ArrayList<String> list = new ArrayList<String>();
+	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//TextView t = (TextView) findViewById(R.id.textView1) ;
-		
-		ListView listView = (ListView) findViewById(R.id.listView1);
-		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-		  "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-		  "Linux", "OS/2" };
+		// TextView t = (TextView) findViewById(R.id.textView1) ;
+
+		listView = (ListView) findViewById(R.id.listView1);
 
 		// Define a new Adapter
 		// First parameter - Context
@@ -31,26 +33,13 @@ public class MainActivity extends Activity {
 		// Third parameter - ID of the TextView to which the data is written
 		// Forth - the Array of data
 
-		ArrayList<String> list = new ArrayList() ;
-		for (Alert a : Driver.loadFeed()){
-			list.add(a.getTitle());
-		}
-		
-		//list = Driver.loadFeed();
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-		  android.R.layout.simple_list_item_1, android.R.id.text1, list);
+		// this creates a new AsyncTask to do the "heavy lifting" so our main ui
+		// thread does not slow down
+		new CreateAlerts().execute();
 
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1, list);
 
-		// Assign adapter to ListView
-		listView.setAdapter(adapter);
-		listView.setOnItemClickListener(listView.getOnItemClickListener());
-		
-		
-	
-//		WebView wV = (WebView) findViewById(R.id.webView1);
-//		wV.loadUrl("https://alert.umd.edu/rssfeed.php");
-	
 	}
 
 	@Override
@@ -60,4 +49,28 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private class CreateAlerts extends AsyncTask<Void, Void, ArrayList<String>> {
+
+		@Override
+		protected ArrayList<String> doInBackground(Void... params) {
+			
+		
+			Driver.loadFeed(DEFAULT_RSS_URL);// creates the list of alerts and stores it in a
+								// static Array list
+
+			// loop through all Alerts and add the alert title to the list that will be appended to the list adapter
+			for (Alert a : Driver.Alertlist) {
+				list.add(a.getTitle());
+			}
+			return list;
+		}
+
+		protected void onPostExecute(ArrayList<String> list) {
+
+			// Assign adapter to ListView
+			listView.setAdapter(adapter);
+			listView.setOnItemClickListener(listView.getOnItemClickListener());
+
+		}
+	}
 }
