0ef10bf7e0dfbfc1fbfd6afbfc1ed1bc61cfad33
kemaric@terpmail.umd.edu <kemaric@terpmail.umd.edu@92224ab3-2fd2-0d66-1742-4fb4694664a7>
2013-04-23 00:37:27 +0000

    Added the Enums of different Alert types.

--- trunk/MadAlerts/src/com/example/madalerts/AlertType.java
+++ trunk/MadAlerts/src/com/example/madalerts/AlertType.java
@@ -2,6 +2,6 @@ package com.example.madalerts;
 
 public enum AlertType {
 
-	WEATHER,VIOLENT,NONVIOLENT,OTHER
+	WEATHER,SAFETY,TRAFFIC,BASIC
 	//The OTHER type is for alerts that don't match any of the other 3 types.
 }
